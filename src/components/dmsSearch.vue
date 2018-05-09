feae4330a7693c239b03c389d6353db62d88697c
Tony.Hou <Tony.hou@carlt.com.cn>
2018-05-09 19:11:35 +0800

    加入elment日期插件

--- src/components/dmsSearch.vue
+++ src/components/dmsSearch.vue
@@ -11,7 +11,7 @@
                 <p>
                     <span>{{item.title}}</span>
                     <span>{{item.selected}}</span>
-                    <select name="" id="" v-model="item['selected']">
+                    <select name="" id="" v-model="item['selected']" @change="conditionChange(item.selected,index)">
                         <option value="" disabled>请选择</option>
                         <option value="include">包含</option>
                         <option value="just">正好是</option>
@@ -37,7 +37,7 @@
             <div v-if="searchGroup[index].type==='selectes'">
                 <p>
                     <span>{{item.title}}</span>
-                    <select name="" id="" v-model="item['selected']">
+                    <select name="" id="" v-model="item['selected']" @change="conditionChange(item.selected,index)">
                         <option value="" disabled>请选择</option>
                         <option value="just">正好是</option>
                         <option value="not">不是</option>
@@ -66,7 +66,7 @@
                  <p>
                     <span>{{item.title}}</span>
                     <span>{{item.selected}}</span>
-                    <select name="" id="" v-model="item['selected']">
+                    <select name="" id="" v-model="item['selected']" @change="conditionChange(item.selected,index)">
                         <option value="" disabled>请选择</option>
                         <option value="equal">等于</option>
                         <option value="lt">小于</option>
@@ -98,28 +98,57 @@
             <div v-if="searchGroup[index].type==='dates'">
                 <div>
                     <span>{{item.title}}</span>
-                    <select name="" id="">
-                        <option value="">等于</option>
-                        <option value="">小于</option>
-                        <option value="">小于等于</option>
-                        <option value="">大于</option>
-                        <option value="">大于等于</option>
-                        <option value="">介于</option>
-                        <option value="">之一</option>
+                    <select name="" id="" v-model="item.selected" @change="conditionChange(item.selected,index)">
+                        <option value="" disabled>请选择</option>
+                        <option value="equal">等于</option>
+                        <option value="lt">小于</option>
+                        <option value="lte">小于等于</option>
+                        <option value="gt">大于</option>
+                        <option value="gte">大于等于</option>
+                        <option value="between">介于</option>
+                        <option value="oneOf">之一</option>
                     </select>
                     <button @click="removeSearchOption(index)">移除搜索单元</button>
                 </div>
                 <div>
-                    <input type="date" name="" id="">
+                    <div v-for="(ls,ide) in item.list" :key="ide">
+                        <div style="position:relative">
+                            <!-- <div v-if="item.selected==='between'"> -->
+                            <div v-show="showDateRange">
+                                <el-date-picker
+                                v-model="ls.dateRange"
+                                size='small'
+                                type="daterange"
+                                range-separator="至"
+                                value-format="yyyy-MM-dd"
+                                start-placeholder="开始日期"
+                                end-placeholder="结束日期">
+                                </el-date-picker>
+                            </div>
+                            <!-- <div v-if="item.selected !='between'"> -->
+                            <div v-show="!showDateRange">
+                                <!-- <input type="date" v-model="ls.date" name="" id=""> -->
+                                <el-date-picker
+                                v-model="ls.date"
+                                size='small'
+                                type="date"
+                                value-format="yyyy-MM-dd"
+                                placeholder="选择日期">
+                                </el-date-picker>
+                            </div>
+                        </div>
+                         <button @click="deleOneOf(index,ide)" v-show="item.list.length>1">×</button>
+                    </div>
+
                     <!-- 之一的话需要添加 -->
-                    <button>+添加</button>
+                    <button v-show="item.selected==='oneOf'" @click="addOneOf(index)" >+添加</button>
                 </div>
             </div>
             <!-- if 车系车型车款  四级联动-->
             <div v-if="searchGroup[index].type === 'carAbout'">
                 <div>
                     <span>{{item.title}}</span>
-                    <select name="" id="" v-model="item['selected']">
+                    <select name="" id="" v-model="item['selected']" @change="conditionChange(item.selected,index)">
                         <option value="">请选择</option>
                         <option value="include">包含</option>
                         <option value="just">正好是</option>
@@ -135,28 +164,28 @@
                     <div>
                         <div v-for="(ls,ide) in item.list" :key="ide">
                             <!-- 品牌 -->
-                            <select name="" id="" @change="carBrandChange(index,ide)" v-model="item.list[ide].brand">
+                            <select name="" id="" @change="carBrandChange(index,ide,item.list[ide].brandID)" v-model="item.list[ide].brandID">
                                 <option value="">请选择</option>
                                 <!-- for 遍历要选的内容 -->
                                 <option v-for="brand in carBrand" :key="brand.id" :value="brand.id">{{brand.title}}</option>
                             </select>
                             <!-- 车系 -->
-                            <select name="" id="" @change="carSeriesChange(index,ide)" v-model="item.list[ide].series">
+                            <select name="" id="" @change="carSeriesChange(index,ide,item.list[ide].seriesID)" v-model="item.list[ide].seriesID">
                                 <option value="">请选择</option>
                                 <!-- for 遍历要选的内容 -->
-                                <option :value="series" v-for="series in carSeries" :key="series.id">{{series.title}}</option>
+                                <option :value="series.id" v-for="series in carSeries[ide]" :key="series.id">{{series.title}}</option>
                             </select>
                             <!-- 车型 -->
-                            <select name="" id="" @change="carModelChange(index,ide)" v-model="item.list[ide].model">
+                            <select name="" id="" @change="carModelChange(index,ide,item.list[ide].modelID)" v-model="item.list[ide].modelID">
                                 <option value="">请选择</option>
                                 <!-- for 遍历要选的内容 -->
-                                <option v-for="model in carModel" :value="model.id" :key="model.id">{{model.title}}</option>
+                                <option v-for="model in carModel[ide]" :value="model.id" :key="model.id">{{model.title}}</option>
                             </select>
                             <!-- 车款 -->
-                            <select name="" id="" v-model="item.list[ide].style" >
+                            <select name="" id="" v-model="item.list[ide].styleID" >
                                 <option value="">请选择</option>
                                 <!-- for 遍历要选的内容 -->
-                                <option v-for="style in carStyle" :key="style.id"  :value="style.id">{{style.title}}</option>
+                                <option v-for="style in carStyle[ide]" :key="style.id"  :value="style.id">{{style.title}}</option>
                             </select>
                             <button @click="deleOneOf(index,ide)" v-show="item.list.length>1">×</button>
                         </div>
@@ -211,6 +240,7 @@
         name:'dmsSearch',
         data(){
             return {
+                showDateRange:true, //修改element样式bug
                 carBrand:[], // 汽车品牌列表
                 carSeries:[], // 车系列表
                 carModel:[], // 车型列表
@@ -219,12 +249,12 @@
                 cityData:[], // 城市信息列表
                 areaData:[], // 区县信息列表
                 searchGroup:[ //储存搜索列表
-                    { title:'工单号',type:'text',list:[{}],selected:''},
+                    // { title:'工单号',type:'text',list:[{}],selected:''},
                     // {title:'送修人',type:'selectes',list:[{}],selected:''},
                     // {title:'金额',type:'numbers',list:[{}],selected:''},
-                    // {title:'购买日期',type:'dates'},
-                    {title:'车型车款',type:'carAbout',list:[{brand:'',series:'',model:'',style:''}]},
-                    {title:'联系地址',type:'address',list:{pro:'',city:'',area:'',address:''}},  // 省 暂时不用列表 后面可能会改回来
+                    {title:'购买日期',type:'dates',list:[{}],selected:"equal"}, // list 数组里至少有ge空对象
+                    // {title:'车型车款',type:'carAbout',list:[{brandID:'',seriesID:'',modelID:'',styleID:''}]},
+                    // {title:'联系地址',type:'address',list:{pro:'',city:'',area:'',address:''}},  // 省 暂时不用列表 后面可能会改回来
                 ],
                 // searchlist:[
                 //     {
@@ -242,7 +272,7 @@
         methods:{
             // 按条件查询
             searchMessage(){
-
+                console.log('查询条件',this.searchGroup)
             },
             // 保存查询项目
             saveSearchGroup(){
@@ -326,12 +356,47 @@
             },
             // 之一 时添加选项
             addOneOf(index){
-                this.searchGroup[index].list.push({});
+                if(this.searchGroup[index].type==='carAbout'){
+                    console.log('add careAbout');
+                    this.searchGroup[index].list.push({brandID:'',seriesID:'',modelID:'',styleID:''});
+                }else{
+                    this.searchGroup[index].list.push({});
+                }
+
             },
             // 移除搜索项乘员
             removeSearchOption(index){
                 this.searchGroup.splice(index,1)
                 console.log('移除第',index,'个')
+            },
+            // 搜索条件改变时
+            conditionChange(selet,index){
+                console.log('选择了',selet,'当前单元的索引是',index,'索引用于区分哪个改变')
+                // 如果不是之一的时候需要对searchGroup做处理
+                if(selet !='oneOf'){
+                    console.log('之一');
+                    this.searchGroup[index].list.splice(1)
+                    console.log(this.searchGroup[index])
+                    // console.log(this.searchGroup[index].list)
+                }
+                // 处理如果是between
+                if(selet ==='between'){
+                    console.log('之间')
+                    // 日期之间
+                    if(this.searchGroup[index].type ==="dates"){
+                        console.log('日期之间');
+                        this.searchGroup[index].list = [{}];
+                         this.showDateRange =!this.showDateRange ; // 修改enelemt样式bug
+                    }
+                }else{
+                    // 不是日期之间
+                    if(this.searchGroup[index].type ==="dates"){
+                        console.log('日期不是之间');
+                        // this.searchGroup[index].list = [{}];  //直接置空对象
+                        delete this.searchGroup[index].list[0].dateRange; // 或 清空队形属性（好处时可以保留日期里的结果
+                         this.showDateRange =!this.showDateRange ;// 修改enelemt样式bug
+                    }
+                }
             },
             // 获取汽车品牌信息
             getBrandData(){
@@ -340,58 +405,60 @@
                 this.carBrand = [{id:1,title:'奥迪'},{id:2,title:'奔驰'}];
             },
             // 获取车系数据
-            getSeriesData(){
+            getSeriesData(ide){
                 // 假数据
-                this.carSeries = [{id:101,title:'Q'},{id:102,title:'XXL'},{id:103,title:'XXXL'}]
+                this.carSeries[ide] = [{id:101,title:'Q'},{id:102,title:'XXL'},{id:103,title:'XXXL'}];
             },
             // 获取车型数据
-            getModelData(){
+            getModelData(ide){
                 // 假数据
-                this.carModel = [{id:101011,title:'车型1'},{id:1022,title:'车型2'}]
+                this.carModel[ide] = [{id:101011,title:'车型1'},{id:1022,title:'车型2'}];
             },
             // 获取车款数据
-            getStyleData(){
-                this.carStyle = [{id:11012,title:'车款1'},{id:11013,title:'车款2'},{id:11014,title:'车款4'},{id:11015,title:'车款5'},]
+            getStyleData(ide){
+                this.carStyle[ide] = [{id:11012,title:'车款1'},{id:11013,title:'车款2'},{id:11014,title:'车款4'},{id:11015,title:'车款5'}];
             },
             // 汽车品牌改变时
-            carBrandChange(index,ide){
-                console.log('汽车品牌改变');
+            carBrandChange(index,ide,brandId){
+                console.log('汽车品牌改变',brandId);
                  // 0. 清空后面 select option
-                this.carSeries = [];
-                this.carModel = [];
-                this.carStyle = [];
+                this.carSeries[ide] = [];
+                this.carModel[ide] = [];
+                this.carStyle[ide] = [];
                 // 1.发送请求获取 车系 数据 组件加载时已经获取
-                this.getSeriesData();
+                this.getSeriesData(ide);
                 //2.记录数据
 
                 // 3.初始化 车型 车系 车款
-                this.searchGroup[index].list[ide].series = '';
-                this.searchGroup[index].list[ide].model = '';
-                this.searchGroup[index].list[ide].style = '';
+                this.searchGroup[index].list[ide].seriesID = '';
+                this.searchGroup[index].list[ide].modelID = '';
+                this.searchGroup[index].list[ide].styleID = '';
             },
             // 车系改变时
-            carSeriesChange(index,ide){
+            carSeriesChange(index,ide,seriesId){
+                console.log('车系改变',seriesId)
                 // 0. 清空后面 select option
-                this.carModel = [];
-                this.carStyle = [];
+                this.carModel[ide] = [];
+                this.carStyle[ide] = [];
                 // 1.发送请求获取 车型 数据
-                this.getModelData();
+                this.getModelData(ide);
                 //2.记录数据
 
                 // 3.初始化 车型  车款
-                this.searchGroup[index].list[ide].model = '';
-                this.searchGroup[index].list[ide].style = '';
+                this.searchGroup[index].list[ide].modelID = '';
+                this.searchGroup[index].list[ide].styleID = '';
             },
             // 车型改变时
-            carModelChange(index,ide){
+            carModelChange(index,ide,modelId){
+                console.log('车型改变',modelId)
                  // 0. 清空后面 select option
-                this.carStyle = [];
+                this.carStyle[ide] = [];
                 // 1.发送请求获取 车型 数据
-                this.getStyleData();
+                this.getStyleData(ide);
                 //2.记录数据
 
                 // 3.初始化 车型  车款
-                this.searchGroup[index].list[ide].style = '';
+                this.searchGroup[index].list[ide].styleID = '';
             },
         },
         mounted(){
--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 import App from './App'
 import router from './router'
-import iView from 'iview'
-import 'iview/dist/styles/iview.css'; 
+// import iView from 'iview'
+// import 'iview/dist/styles/iview.css';
+Vue.use(ElementUI);
 Vue.config.productionTip = false
-Vue.use(iView);
+// Vue.use(iView);
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
